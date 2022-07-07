9d62f50b11ca7a1e60eb1cbd6cb0c24d140ec302
zokeez <birukov.alex@gmail.com>
2022-07-07 13:16:28 +0500

    --

--- app/src/main/java/com/tomato/course2_1activity/AnotherActivity.kt
+++ app/src/main/java/com/tomato/course2_1activity/AnotherActivity.kt
@@ -2,10 +2,53 @@ package com.tomato.course2_1activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.tomato.course2_1activity.databinding.ActivityAnotherBinding
+import com.tomato.course2_1activity.databinding.ActivityMainBinding
 
 class AnotherActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAnotherBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_another)
+        binding = ActivityAnotherBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.d(MainActivity.TAG, "onCreate, savedInstanceState = $savedInstanceState")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState")
+    }
+
+    companion object{
+        val TAG = AnotherActivity::class.simpleName
     }
 }
--- app/src/main/java/com/tomato/course2_1activity/MainActivity.kt
+++ app/src/main/java/com/tomato/course2_1activity/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.tomato.course2_1activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import com.tomato.course2_1activity.databinding.ActivityMainBinding
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,13 +19,49 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.btn1.setOnClickListener {
-
+            startActivity(Intent(this, AnotherActivity::class.java))
         }
         binding.btn2.setOnClickListener {
-
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1)
         }
         binding.btn3.setOnClickListener {
-
+            moveTaskToBack(false)
         }
+
+        Log.d(TAG, "onCreate, savedInstanceState = $savedInstanceState")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState")
+    }
+
+    companion object{
+        val TAG = MainActivity::class.simpleName
     }
 }
